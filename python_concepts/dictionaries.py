01526d4ccf8014b30fa2cdcc7fdc434c3be76c04
MojisolaaaO <mojisolaotusheso@gmail.com>
2024-09-10 09:59:26 +0100

    create: dictionaries

---
+++ python_concepts/dictionaries.py
@@ -0,0 +1,13 @@
+"""
+Dictionaries are a type of pyton data collection that store
+the data in key value pairs
+
+the keys are made of strings, integers or tuples and need to be both
+unique and mutable
+
+the values can be set to any data types
+any number of key value pairs can be contained in the dictionary
+an empty dictionary is made by assigning a dic name with nothing in the curly braces [}
+
+
+"""
